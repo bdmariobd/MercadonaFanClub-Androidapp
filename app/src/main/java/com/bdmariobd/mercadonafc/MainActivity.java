fb6768ef9e9acb0b1fa790471e144e09cd131005
Mario Blanco <mario10_bd@hotmail.com>
2023-10-29 11:04:57 +0100

    Initial commit

---
+++ app/src/main/java/com/bdmariobd/mercadonafc/MainActivity.java
@@ -0,0 +1,16 @@
+package com.bdmariobd.mercadonafc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.color.DynamicColors;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
