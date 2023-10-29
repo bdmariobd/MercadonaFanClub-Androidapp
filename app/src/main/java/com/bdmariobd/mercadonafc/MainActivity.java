62f22f7c514841921ac6476e950c74d790d9faba
Mario Blanco <mario10_bd@hotmail.com>
2023-10-29 15:55:00 +0100

    base layout for the app

--- app/src/main/java/com/bdmariobd/mercadonafc/MainActivity.java
+++ app/src/main/java/com/bdmariobd/mercadonafc/MainActivity.java
@@ -1,16 +1,36 @@
 package com.bdmariobd.mercadonafc;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-import com.google.android.material.color.DynamicColors;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.bdmariobd.mercadonafc.fragments.AccountFragment;
+import com.bdmariobd.mercadonafc.fragments.HomeFragment;
+import com.bdmariobd.mercadonafc.fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    final Fragment homeFragment = new HomeFragment();
+    final Fragment searchFragment = new SearchFragment();
+    final Fragment accountFragment = new AccountFragment();
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+                    if (item.getItemId() == R.id.menu_home) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, homeFragment).commit();
+                    } else if (item.getItemId() == R.id.menu_search) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, searchFragment).commit();
+                    } else if (item.getItemId() == R.id.menu_account) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, accountFragment).commit();
+                    }
+                    return true;
+                }
+        );
     }
 }
